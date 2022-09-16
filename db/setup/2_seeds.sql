4994040f67e9382bbdd071cd32a50ecd01a397c9
Camila Pessoa <caape13@hotmail.com>
2022-09-16 15:06:33 +0100

    change files name and move seeds to Back

--- db/setup/2_seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO habits(habitname, date_done, period, frequency)
-VALUES
-    ('drink water', [20220101, 20220102, 20220103], 1, 5)
-    ('gym', [20220105, 20220107, 20220108], 1, 3)
-    ('basketball', [20220211, 20220312, 20220320], 2, 2)
