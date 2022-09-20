201983f6484b1ca54e70e32d53a89031afc5a858
Matthew Lo <matthewlol316@gmail.com>
2022-09-20 22:08:44 +0100

    users able to login and show index.html

---
+++ db/migrations/2_create_users.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+    id serial PRIMARY KEY,
+    user_name varchar(255) NOT NULL UNIQUE,
+    email varchar(100) NOT NULL UNIQUE,
+    password_digest varchar(500) NOT NULL
+)
