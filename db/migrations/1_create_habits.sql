0bc0c342a80a2b9dab3f7c188363c698a28dd00b
Camila Pessoa <caape13@hotmail.com>
2022-09-21 06:42:41 +0100

    add test files and install coverage

---
+++ db/migrations/1_create_habits.sql
@@ -0,0 +1,11 @@
+
+DROP TABLE IF EXISTS habits;
+
+CREATE TABLE habits(
+    id serial PRIMARY KEY,
+    habit_name varchar(255) NOT NULL UNIQUE,
+    date_complete integer[],
+    period int NOT NULL,
+    frequency int NOT NULL,
+    user_id integer
+);
