d705168bf761cddf41bcee02314b95d109bd7a36
Matthew Lo <matthewlol316@gmail.com>
2022-09-22 22:24:49 +0100

    change datatype to DATE

--- db/migrations/1_create_habits.sql
+++ db/migrations/1_create_habits.sql
@@ -3,9 +3,10 @@ DROP TABLE IF EXISTS habits;
 CREATE TABLE habits(
     id serial PRIMARY KEY,
     habit_name varchar(255) NOT NULL UNIQUE,
-    date_complete integer[],
+    date_complete DATE,
     period int NOT NULL,
     frequency int NOT NULL,
     frequency_done integer NOT NULL,
+    streak integer,
     user_id integer
 );
